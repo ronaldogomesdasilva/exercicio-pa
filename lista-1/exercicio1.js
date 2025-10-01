f7f1767834c84efdb0cf6b0880d5bb01f5fb9215
Ronaldo Gomes da Silva <rnld.gomes@gmail.com>
2025-10-01 20:11:13 -0300

    Add files via upload

---
+++ lista-1/exercicio1.js
@@ -0,0 +1,9 @@
+let input =  require('readline-sync');
+
+let n1 = input.questionInt("Digite um numero: ")
+
+n1 = n1+5
+
+console.log("o numero digitado é: ", n1)
+
+
---
+++ lista-1/exercicio2.js
@@ -0,0 +1,10 @@
+let imput = require ("readline-sync")
+
+let n1 = input.questionInt("Digite um numero: (n1) ..")
+
+n1 = (n1-3)*2;
+
+console.log (numero 1 = , N1)
+
+
+
